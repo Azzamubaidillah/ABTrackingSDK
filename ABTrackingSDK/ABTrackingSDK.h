c28a33691ec94cee368ddf8ffc3c903147247d35
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-11-14 09:51:47 +0700

    Initial Commit

---
+++ ABTrackingSDK/ABTrackingSDK.h
@@ -0,0 +1,18 @@
+//
+//  ABTrackingSDK.h
+//  ABTrackingSDK
+//
+//  Created by Azzam Ubaidillah on 14/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ABTrackingSDK.
+FOUNDATION_EXPORT double ABTrackingSDKVersionNumber;
+
+//! Project version string for ABTrackingSDK.
+FOUNDATION_EXPORT const unsigned char ABTrackingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABTrackingSDK/PublicHeader.h>
+
+
